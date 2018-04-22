d52039fc74ede23d6ece472a4039805a621f8277
Liu Teng Hou <liutenghou@liutenghou-air11.local>
2018-04-22 14:47:00 -0700

    added elements to custom cell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Liu Teng Hou on 4/22/18.
+//  Copyright © 2018 Blue Anchor. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
